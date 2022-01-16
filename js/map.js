dff534a213caeca6b15f1df7369cd2fc91c3ef83
oleksandrasmith <91082509+oleksandrasmith@users.noreply.github.com>
2022-01-15 22:26:51 -0500

    add map

--- js/map.js
+++ js/map.js
@@ -18,3 +18,16 @@ editBtn.addEventListener("click", () => {
   profileUsername.toggleAttribute("disabled");
   editBtn.classList.toggle("editing");
 });
+// Map Location
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FzaGFwb28iLCJhIjoiY2t5Z2p4MDRxMDllMjJwb2x3Z2p3eWlzNCJ9.ZpZ5fmYIFxhljKiga7DSXw';
+const map = new mapboxgl.Map({
+    container: 'map', 
+    style: 'mapbox://styles/mapbox/streets-v11', 
+
+});
+// NavigationControl/Direction
+
+const nav = new mapboxgl.NavigationControl({
+  visualizePitch: true
+})
+map.addControl(nav, 'bottom-right')
