794115d69d3e6f166a3d154c2e0b840004050863
doki333 <duckky333@gmail.com>
2022-01-19 01:49:02 +0900

    resize search bar for mobile screen and delete username edit button

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 const profileBtn = document.querySelector(".profile_info");
 const closeBtn = document.querySelector(".close_btn");
-const editBtn = document.querySelector(".edit_btn");
 const profileUsername = document.querySelector("#profile_username");
 const profileModal = document.querySelector(".profile_modal");
 
@@ -11,13 +10,8 @@ profileBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   profileModal.classList.remove("active");
   profileUsername.disabled = true;
-  editBtn.classList.remove("editing");
 });
 
-editBtn.addEventListener("click", () => {
-  profileUsername.toggleAttribute("disabled");
-  editBtn.classList.toggle("editing");
-});
 // Map Location
 openchargemap_accessToken = "ba0bdea1-7220-4e1c-a3e1-60deca08d26a";
 mapboxgl.accessToken =
